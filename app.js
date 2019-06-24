a0fbab0669722917511cc2aa78fc57d1739e0262
Wojciech Czarnocki <30754368+caren1@users.noreply.github.com>
2019-06-24 22:20:38 +0200

    Finished the functionality of creating a custom lists using express route parameters.

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ const workout = new Item({
 
 const defaultItems = [wakeUp, work, workout];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 
 app.get("/", function(req, res) {
@@ -65,45 +71,88 @@ app.get("/", function(req, res) {
 
 });
 
+app.get("/:customListName", function(req, res) {
 
-app.post("/", function(req, res) {
+      const customListName = req.params.customListName;
 
-  const itemName = req.body.newItem;
+      List.findOne({
+        name: customListName
+      }, function(err, foundList) {
+        if (!err) {
+          if (!foundList) {
+            const list = new List({
+              name: customListName,
+              items: defaultItems
+            });
 
-  const item = new Item({
-    name: itemName
-  });
+            list.save();
+            res.redirect("/"+customListName);
+          } else {
+            res.render("list", {
+              listTitle: foundList.name,
+              newListItems: foundList.items
+            });
+          }
+        }
+      });
+    });
 
-  item.save();
 
-  res.redirect("/");
 
-});
+    //       console.log("There is already a list with such name.");
+    //     }else{
+    //       const list = new List({
+    //         name: customListName,
+    //         items: defaultItems
+    //       });
+    //
+    //       list.save();
+    //
+    //       res.redirect("/"+customListName);
+    //     }
+    //   });
+    // });
 
-app.post("/delete", function(req, res){
-  const checkedItemId = req.body.checkbox;
 
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if(err){
-      console.log(err);
-    }else{
+    app.post("/", function(req, res) {
+
+      const itemName = req.body.newItem;
+
+      const item = new Item({
+        name: itemName
+      });
+
+      item.save();
+
       res.redirect("/");
-      console.log("Successfully deleted the checked item");
-    }
-  });
-});
 
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Today",
-    newListItems: workItems
-  });
-});
+    });
 
-app.get("/about", function(req, res) {
-  res.render("about");
-});
+    app.post("/delete", function(req, res) {
+      const checkedItemId = req.body.checkbox;
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+      Item.findByIdAndRemove(checkedItemId, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect("/");
+          console.log("Successfully deleted the checked item");
+        }
+      });
+    });
+
+
+    // app.get("/work", function(req, res) {
+    //   res.render("list", {
+    //     listTitle: "Today",
+    //     newListItems: workItems
+    //   });
+    // });
+
+    app.get("/about", function(req, res) {
+      res.render("about");
+    });
+
+    app.listen(3000, function() {
+      console.log("Server started on port 3000");
+    });
