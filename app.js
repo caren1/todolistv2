2aa5a04c3a6c8f17647af8caa61867674717acfe
Wojciech Czarnocki <30754368+caren1@users.noreply.github.com>
2019-06-26 19:26:50 +0200

    Added MongoDB Atlas configuration, preparing for Heroku deployment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-wojciech:Test123@todolist-uyc2n.mongodb.net/todolistDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
